0c769da243539641207bead9620fb310b40a36da
Egor <ekurito@gmail.com>
2019-10-25 12:58:14 +0300

    add 1.6 -  Mapping Keys to Multiple Values in a Dictionary

--- 1.6.py
+++ 1.6.py
@@ -3,3 +3,36 @@ Mapping Keys to Multiple Values in a Dictionary
 Problem:
 You want to make a dictionary that maps keys to more than one value (a so-called “multidict”).
 '''
+
+# Use list if you want to preserve the insertion order of the items
+from collections import defaultdict
+d = {
+    'a': [1, 2, 4],
+    'b': [4, 5],
+}
+
+# Use a set if you want to eliminate duplicates
+e = {
+    'a': {1, 2, 3},
+    'b': {4, 5}
+}
+
+
+d = defaultdict(list)
+d['a'].append(1)
+
+e = defaultdict(set)
+e['a'].append(1)
+
+
+# Dirty code
+d = {}
+for key, value in pairs:
+    if key not in d:
+        d[key] = []
+    d[key].append(value)
+
+# Clean code
+d = defaultdict(list)
+for key, value in pairs:
+    d[key].append(value)
